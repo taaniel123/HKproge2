75c6cb9416a5075c6bf677324b656d1efc918403
Taaniel <taaniel@tlu.ee>
2021-09-26 20:20:21 +0300

    Esimene tund valmis

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,25 @@ const app: Application = express();
 const port: number = 3000;
 app.use(express.json());
 
-const db = { 
+
+interface User {
+    id: number;
+    firstName: string;
+    lastName: string;
+};
+
+
+interface Db {
+    users: User[];
+};
+  
+
+const db: Db = { 
     users: [
     {
       id: 1,
       firstName: 'Taaniel',
-      lastName: 'Javascript',
+      lastName: 'Typescript',
     },
     {
       id: 2,
@@ -19,17 +32,20 @@ const db = {
   ]
 };
 
+
 app.get('/api', (req: Request, res: Response) => {
     res.status(200).json({
         message: 'Hello world!',
     });
 });
 
+
 app.get('/users', (req: Request, res: Response) => {
     res.status(200).json({
         users: db.users,
     });
 });
+
 
 app.get('/users/:id', (req: Request, res: Response) => {
     const id: number = parseInt(req.params.id);
@@ -38,6 +54,7 @@ app.get('/users/:id', (req: Request, res: Response) => {
       user,
     });
 });
+
 
 app.post('/users', (req: Request, res: Response) => {
     const { firstName, lastName } = req.body;
@@ -53,6 +70,54 @@ app.post('/users', (req: Request, res: Response) => {
     });
 });
 
+
+app.delete('/users/:id', (req: Request, res: Response) => {
+    const id: number = parseInt(req.params.id, 10);
+    if (!id) {
+      return res.status(400).json({
+        error: 'No valid id provided',
+      });
+    }
+    const index = db.users.findIndex((element) => element.id === id);
+    if (index < 0) {
+      return res.status(400).json({
+        message: `User not found with id: ${id}`,
+      });
+    }
+    db.users.splice(index, 1);
+    return res.status(204).send();
+});
+
+
+app.patch('/users/:id', (req: Request, res: Response) => {
+    const id: number = parseInt(req.params.id, 10);
+    const { firstName, lastName } = req.body;
+    if (!id) {
+      return res.status(400).json({
+        error: 'No valid id provided',
+      });
+    }
+    if (!firstName && !lastName) {
+      return res.status(400).json({
+        error: 'Nothing to update',
+      });
+    }
+    const index = db.users.findIndex((element) => element.id === id);
+    if (index < 0) {
+      return res.status(400).json({
+        error: `No user found with id: ${id}`,
+      });
+    }
+    if (firstName) {
+      db.users[index].firstName = firstName;
+    }
+    if (lastName) {
+      db.users[index].lastName = lastName;
+    }
+    return res.status(204).send();
+});
+
+  
 app.listen(port, () => {
     console.log('Server jookseb');
 });
